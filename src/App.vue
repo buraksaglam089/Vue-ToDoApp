0c31e5b37d46d83109c191323601f53b98e017fe
buraksaglam089 <bsaglam089@gmail.com>
2023-02-20 15:30:56 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <HeaderComponent @add:card="onAddCard"></HeaderComponent>
+    <AppCard :cards="cards"></AppCard>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import AppCard from './components/AppCard.vue';
+
+export default {
+    data() {
+        return {
+            cards: []
+        };
+    },
+    methods: {
+        onAddCard(card) {
+            this.cards.push(card);
+        }
+    },
+    components: {
+        HeaderComponent,
+        AppCard
+    }
+};
+</script>
+
+<style></style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="grid">
+        <div v-for="card in cards" :key="card" class="col-3">
+            <Card>
+                <template #header>
+                    {{ card.title }}
+                </template>
+                <template #content>
+                    {{ card.description }}
+                </template>
+            </Card>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        cards: {
+            type: Array,
+            default: () => []
+        }
+    }
+};
+</script>
